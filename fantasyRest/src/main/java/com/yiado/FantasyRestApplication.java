65365148390ffd5dc87b2b82892b4c7a375c42e4
YmendezM <ysmendezm@gmail.com>
2021-07-16 13:16:32 -0400

    First commit

---
+++ fantasyRest/src/main/java/com/yiado/FantasyRestApplication.java
@@ -0,0 +1,13 @@
+package com.yiado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyRestApplication.class, args);
+	}
+
+}
---
+++ fantasyRest/src/test/java/com/yiado/FantasyRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
